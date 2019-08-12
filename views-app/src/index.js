faf230fb1af6764bebb3354f329282ebafc9e838
Paul Dittamo <>
2019-08-12 17:37:03 -0400

    delete unused mock data

--- views-app/src/index.js
+++ views-app/src/index.js
@@ -38,51 +38,6 @@ var moviesProm = new Promise((resolve, reject) => {
 	});
 });
 
-const movies = [{
-	id: 1,
-	header: "featured",
-	title: "Shark boy",
-	body: "This is a mediocre movie",
-}, 
-{
-	id: 2,
-	header: "featured",
-	title: "Lava Girl",
-	body: "This is a better movie",
-
-}, 
-{
-	id: 3,
-	header: "featured",
-	title: "Lava Girl",
-	body: "This is a better movie",
-},
-{
-	id: 4,
-	header: "featured",
-	title: "Lava Girl",
-	body: "This is a better movie",
-},
-{
-	id: 5,
-	header: "featured",
-	title: "Lava Girl",
-	body: "This is a better movie, Now that we covered all the “easy” things let’s move into the “real world debugging” — working with breakpoints. A breakpoint is an instruction in code which will stop all the execution and fire up a debugger. There are 2 ways of setting up breakpoints that you will find handy",
-},
-{
-	id: 6,
-	header: "featured",
-	title: "Lava Girl",
-	body: "This is a better movie",
-},
-{
-	id: 7,
-	header: "featured",
-	title: "Lava Girl",
-	body: "This is a better movie",
-}]
-
-
 //Note that the class names and the elements it renders must be the same
 class App extends Component {
 	constructor(props){
